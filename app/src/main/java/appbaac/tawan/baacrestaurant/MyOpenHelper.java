6678b032f694845137e5d8e70b2ebe28ad25fd91
tawan <pumjam55@hotmail.com>
2015-10-19 13:07:34 +0700

    start GitHub

---
+++ app/src/main/java/appbaac/tawan/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,18 @@
+package appbaac.tawan.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 19/10/2015.
+ */
+public class MyOpenHelper {
+
+
+    //Explicit
+    private static  final  String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public MyOpenHelper(Context context) {   // กด alt+insert เลือก Constructor
+    }   //Constructor
+}   // Main Class นี่คือ คลาสหลัก นะจ๊ะ
