5e64ba1ebbc12c162fecaf2ec7b8aba7ccb479e9
tawan <pumjam55@hotmail.com>
2015-10-19 16:09:01 +0700

    Finish Day1

--- app/src/main/java/appbaac/tawan/baacrestaurant/MainActivity.java
+++ app/src/main/java/appbaac/tawan/baacrestaurant/MainActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
-    private  UserTABLE objUserTABLE;
+    private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +15,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Create & Connected Database
-        createAndConnected();   // if error alt+enter จะสร้าง constructor auto
+        createAndConnected();   // if error red do alt+enter จะสร้าง constructor auto
+
+        //Tester Add New Value
+        testerAdd();
 
     }   //Main Method
+
+    private void testerAdd() {
+        objUserTABLE.addNewUser("testUser", "testPassword", "ทดสอบชื่อภาษาไทย");
+        objFoodTABLE.addNewFood("ชื่ออาหาร", "testSource", "123");
+    }
 
     private void createAndConnected() {
 
         objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
     }
 
 }   // Main Class
--- app/src/main/java/appbaac/tawan/baacrestaurant/UserTABLE.java
+++ app/src/main/java/appbaac/tawan/baacrestaurant/UserTABLE.java
@@ -1,5 +1,6 @@
 package appbaac.tawan.baacrestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,12 @@ public class UserTABLE {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String USER_TABLE = "userTABLE";
+    public static final String COLUMN_ID_USER = "_id";
+    public static final String COLUMN_USER = "User";
+    public static final String COLUMN_PASSWORD = "Password";
+    public static final String COLUMN_NAME = "Name";
+
     public UserTABLE(Context context) {
 
         objMyOpenHelper = new MyOpenHelper(context);
@@ -19,5 +26,15 @@ public class UserTABLE {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }   // Constructor
+
+    public long addNewUser(String strUser, String strPassword, String strName) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_USER, strUser);
+        objContentValues.put(COLUMN_PASSWORD, strPassword);
+        objContentValues.put(COLUMN_NAME, strName);
+
+        return writeSqLiteDatabase.insert(USER_TABLE, null, objContentValues);
+    }
 
 }   // Main Class
